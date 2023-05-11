d5fba1b392046f27cb1eb1c5ed55c8f9c0c268a3
Vladimir Haiduk <nightshade2903@gmail.com>
2023-05-11 22:21:54 +0300

    completed

--- demo/src/main/java/com/epam/demo/Utils.java
+++ demo/src/main/java/com/epam/demo/Utils.java
@@ -1,14 +1,13 @@
 package com.epam.demo;
 
+
+
 import com.epam.utils.StringUtils;
 
 import java.util.List;
 
 public class Utils {
     public static boolean isAllPositiveNumbers(List<String> args) {
-        for (String arg : args) {
-            if (!StringUtils.isPositiveNumber(arg)) return false;
-        }
-        return true;
+        return args.stream().allMatch(StringUtils::isPositiveNumber);
     }
     }
